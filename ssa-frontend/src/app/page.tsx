907239932c3315e16221f24b280a4ea85b5b7aed
Debbie-alt <funmmy565@gmail.com>
2025-09-17 12:13:15 +0100

    onchain testing init

--- ssa-frontend/src/app/page.tsx
+++ ssa-frontend/src/app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
      
 
-      
+       
     </div>
+    // command for installing langchain - npm install @langchain/openai @langchain/core
+
   );
 }
